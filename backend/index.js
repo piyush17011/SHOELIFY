eddee1f4e242f103c747c1d19028653e76998bbb
piyush17011 <piyushbhalwalkar01@gmail.com>
2026-04-21 15:16:48 +0530

    allowed origins

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ connect();
 // Allow Netlify + Localhost
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://shoelify.onrender.com/',   // <--- CHANGE THIS AFTER DEPLOY
+  'https://shoelify.onrender.com',   // <--- CHANGE THIS AFTER DEPLOY
 ];
 
 app.use(cors({
